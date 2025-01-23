46dbfa1f63bb8bb656099d802f271a77174fb55e
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2025-01-24 01:32:37 +0500

    vercel error

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 function Signup() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 return(
 <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
-export default {
-  plugins: [
-    react(),
-    tailwindcss('./tailwind.config.js'),
-
-  ],
-};
+export default defineConfig({
+  plugins: [react()],
+});
