a97bfdd59a443ad4be10ac6539083e4ea2274289
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2025-01-24 01:20:02 +0500

    error solve

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
+export default {
+  plugins: [
+    react(),
+    tailwindcss('./tailwind.config.js'),
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  plugins:  tailwindcss(),
-
-})
+  ],
+};
